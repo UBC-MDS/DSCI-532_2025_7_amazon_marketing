23d45609866da8a81cb70149a3fa2036731358ae
jdaduica <juliandad2001@hotmail.com>
2025-02-25 14:54:23 -0800

    Update app.py (#28)
    Corrected Imports for script

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 import altair as alt
 import dash_bootstrap_components as dbc
 import dash_vega_components as dvc
-from dash import Dash
+from dash import Dash, dcc, callback, Output, Input
 from vega_datasets import data
 
 
