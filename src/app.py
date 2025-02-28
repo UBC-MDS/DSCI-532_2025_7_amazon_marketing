d332c4645eae2e19b47e58ab9b76335f9d6da6c2
Mavis WONG <mavis.295@gmail.com>
2025-02-28 12:45:40 -0800

    Squashed commit of the following:
    commit 9766069fa233235180e625d01ff31c096228634e
    Author: jdaduica <juliandad2001@hotmail.com>
    Date:   Wed Feb 26 23:31:58 2025 -0800
    
        the left column with the filters and callbacks are set up
    
    commit ef1b9fd6959c6e93bae0ae90da38f960c605c4d8
    Merge: 0303d48 919879e
    Author: jdaduica <juliandad2001@hotmail.com>
    Date:   Wed Feb 26 21:34:10 2025 -0800
    
        Merge branch 'main' of github.com:UBC-MDS/DSCI-532_2025_7_amazon_marketing
    
    commit 919879e36606851c0ddffe31308f285c3038f4f6
    Author: Mavis Wong <77270525+MavisWong295@users.noreply.github.com>
    Date:   Tue Feb 25 15:37:47 2025 -0800
    
        Src/charts (#32)
    
        * Added data.py for updating processed data
    
        * Update app.py to regenerate processed data
    
    commit 0303d48d9e5d5031568e00e6855d3c9db5edf35e
    Merge: 52d217c 0f3e24b
    Author: jdaduica <juliandad2001@hotmail.com>
    Date:   Tue Feb 25 15:25:02 2025 -0800
    
        Merge branch 'main' of github.com:UBC-MDS/DSCI-532_2025_7_amazon_marketing
    
    commit 52d217c63bfc38722bbfac126238d6020f3dc3a2
    Merge: ba80140 6089e60
    Author: jdaduica <juliandad2001@hotmail.com>
    Date:   Tue Feb 25 15:18:28 2025 -0800
    
        Merge branch 'main' of github.com:UBC-MDS/DSCI-532_2025_7_amazon_marketing
    
    commit ba8014073ca19bce87c6eae8839888222899c709
    Merge: 76dbfac 23d4560
    Author: jdaduica <juliandad2001@hotmail.com>
    Date:   Tue Feb 25 14:54:48 2025 -0800
    
        Merge branch 'main' of github.com:UBC-MDS/DSCI-532_2025_7_amazon_marketing
    
    commit 76dbfac7f0a61f809471ff13933ddb3ec474e109
    Merge: bf85062 dad8538
    Author: jdaduica <juliandad2001@hotmail.com>
    Date:   Tue Feb 25 14:34:30 2025 -0800
    
        Merge branch 'main' of github.com:UBC-MDS/DSCI-532_2025_7_amazon_marketing
    
    commit bf85062f854d540345c904ef1828fce7d5b06740
    Merge: 94b3eed c0c14a9
    Author: jdaduica <juliandad2001@hotmail.com>
    Date:   Tue Feb 25 14:23:54 2025 -0800
    
        Merge branch 'main' of github.com:UBC-MDS/DSCI-532_2025_7_amazon_marketing
    
    commit 94b3eed7dd457dbb278198d17b0931772ea07964
    Merge: a8c2eea 163b982
    Author: jdaduica <juliandad2001@hotmail.com>
    Date:   Tue Feb 25 14:23:30 2025 -0800
    
        Merge branch 'main' of github.com:UBC-MDS/DSCI-532_2025_7_amazon_marketing
    
    commit a8c2eea205a724b0e8dd73d95b9d1f6f5318d514
    Merge: 9f4399d e8c856d
    Author: jdaduica <juliandad2001@hotmail.com>
    Date:   Tue Feb 25 14:16:34 2025 -0800
    
        Merge branch 'main' of github.com:UBC-MDS/DSCI-532_2025_7_amazon_marketing
    
    commit 9f4399d7d892f2b6ad736dde3748d41d1045ecdc
    Merge: 1d42e45 74c36c7
    Author: jdaduica <juliandad2001@hotmail.com>
    Date:   Sat Feb 15 12:51:29 2025 -0800
    
        Merge branch 'main' of github.com:UBC-MDS/DSCI-532_2025_7_amazon_marketing
    
    commit 1d42e45826c4d5a3a659d227b9094109dfb04f60
    Author: jdaduica <juliandad2001@hotmail.com>
    Date:   Sat Feb 15 12:48:52 2025 -0800
    
        change license type

--- src/app.py
+++ src/app.py
@@ -1,22 +1,199 @@
-from dash import Dash, html
+from dash import Dash, html, dcc, Input, Output, callback
 from data import processed_data
+import dash_bootstrap_components as dbc
 
 processed_data()
 
-# Initiatlize the app
-app = Dash(__name__)
-server = app.server 
+# Initialize the app
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 
-# Layout
-app.layout = html.Div('I am alive')
+# Layout 
+app.layout = dbc.Container(
+    [
+        dbc.Row(
+            [
+                # Left column for filters
+                dbc.Col(
+                    [
+                        # title
+                        html.H1("Amazon Prime Dashboard", style={"fontWeight": "bold", "textAlign": "center", "display": "block", "marginBottom": "30px"}),
 
-# Server side callbacks/reactivity
-# ...
+                        # renewal button
+                        html.Label("Renewal Type", style={"fontWeight": "bold", "textAlign": "center", "display": "block", "marginBottom": "10px", "fontSize": "24px"}),
+                        html.Div(
+                            [
+                                dbc.Button("Manual", id="manual-renew", className="me-1", style={"marginBottom": "10px", "width": "100%", "backgroundColor": "#FF9900"}),
+                                dbc.Button("Auto Renew", id="auto-renew", style={"marginBottom": "50px", "width": "100%", "backgroundColor": "#FF9900"}),
+                            ]
 
-# Run the app/dashboard
-if __name__ == '__main__':
+                        ),    
+                        #gender button
+                        html.Label("Gender", style={"fontWeight": "bold", "textAlign": "center", "display": "block", "marginBottom": "10px", "fontSize": "24px"}),
+                        html.Div(
+                            [
+                                dbc.Button("Male", id="male-gender", className="me-1", style={"marginBottom": "10px", "width": "100%", "backgroundColor": "#FF9900"}),
+                                dbc.Button("Female", id="female-gender", style={"marginBottom": "50px", "width": "100%", "backgroundColor": "#FF9900"}),
+                            ]
+                        ),
+                        #age range slider
+                        html.Label("Age Range", style={"fontWeight": "bold", "textAlign": "center", "display": "block", "marginBottom": "10px", "fontSize": "24px"}),
+                        html.Div(
+                            dcc.RangeSlider(
+                                id='age-range-filter',
+                                min=0,
+                                max=100,
+                                step=10,
+                            ),
+                            style={"marginBottom": "50px"} 
+                        ),
+                        # date range buttons
+                        html.Label("Date Range", style={"fontWeight": "bold", "textAlign": "center", "display": "block", "marginBottom": "10px", "fontSize": "24px"}),
+                        html.Div(
+                            [
+                                dbc.Button("1 Month", id="1-month-filter", className="me-1", style={"marginBottom": "10px", "width": "100%", "backgroundColor": "#FF9900"}),
+                                dbc.Button("3 Month", id="3-month-filter", style={"marginBottom": "10px", "width": "100%", "backgroundColor": "#FF9900"}),
+                                dbc.Button("6 Month", id="6-month-filter", style={"marginBottom": "10px", "width": "100%", "backgroundColor": "#FF9900"}),
+                                dbc.Button("All Time", id="All-time-filter", style={"marginBottom": "20px", "width": "100%", "backgroundColor": "#FF9900"}),
+                            ]
+                        ),
+                    ],
+                    width=2,  
+                ),
+                # Middle column 
+                dbc.Col(
+                    [
+                        # Placeholder for middle columns 
+                        # Table here
+                        # ...
+
+                    ],
+                    width=5,  
+                ),
+
+                # Right column 
+                dbc.Col(
+                    [
+                        # Placeholder for right column 
+                        # Graph Here
+                        # ...
+   
+                    ],
+                    width=5,  
+                ),
+            ]
+        )
+    ],
+    fluid=True,
+)
+
+# callback for current users number 
+@callback(
+    Output("current-number-placeholder", "children"),
+    Input("manual-renew", "n_clicks"),
+    Input("auto-renew", "n_clicks"),
+    Input("male-gender", "n_clicks"),
+    Input("female-gender", "n_clicks"),
+    Input("age-range-filter", "value"),
+    Input("1-month-filter", "n_clicks"),
+    Input("3-month-filter", "n_clicks"),
+    Input("6-month-filter", "n_clicks"),
+    Input("All-time-filter", "n_clicks"),
+)
+def update_current_users_number(manual_clicks, auto_renew_clicks, male_clicks, female_clicks, age_range, month1, month3, month6, all_time):
+
+    return updated_current_number
+
+# callback for expiring users number 
+@callback(
+    Output("expiring-number-placeholder", "children"),
+    Input("manual-renew", "n_clicks"),
+    Input("auto-renew", "n_clicks"),
+    Input("male-gender", "n_clicks"),
+    Input("female-gender", "n_clicks"),
+    Input("age-range-filter", "value"),
+    Input("1-month-filter", "n_clicks"),
+    Input("3-month-filter", "n_clicks"),
+    Input("6-month-filter", "n_clicks"),
+    Input("All-time-filter", "n_clicks"),
+)
+def update_expiring_users_number(manual_clicks, auto_renew_clicks, male_clicks, female_clicks, age_range, month1, month3, month6, all_time):
+
+    return expiring_number
+
+# callback for expiring members table
+@callback(
+    Output("expiring-table-placeholder", "children"),
+    Input("manual-renew", "n_clicks"),
+    Input("auto-renew", "n_clicks"),
+    Input("male-gender", "n_clicks"),
+    Input("female-gender", "n_clicks"),
+    Input("age-range-filter", "value"),
+    Input("1-month-filter", "n_clicks"),
+    Input("3-month-filter", "n_clicks"),
+    Input("6-month-filter", "n_clicks"),
+    Input("All-time-filter", "n_clicks"),
+)
+def update_expiring_members_table(manual_clicks, auto_renew_clicks, male_clicks, female_clicks, age_range, month1, month3, month6, all_time):
+
+    return updated_expiring_members_table
+
+processed_data()
+
+# callback for rating overtime graph
+@callback(
+    Output("rating-overtime-graph-placeholder", "children"),
+    Input("manual-renew", "n_clicks"),
+    Input("auto-renew", "n_clicks"),
+    Input("male-gender", "n_clicks"),
+    Input("female-gender", "n_clicks"),
+    Input("age-range-filter", "value"),
+    Input("1-month-filter", "n_clicks"),
+    Input("3-month-filter", "n_clicks"),
+    Input("6-month-filter", "n_clicks"),
+    Input("All-time-filter", "n_clicks"),
+)
+def update_rating_overtime_graph(manual_clicks, auto_renew_clicks, male_clicks, female_clicks, age_range, month1, month3, month6, all_time):
+
+    return updated_rating_overtime_graph
+
+
+# callback for purchase history graph
+@callback(
+    Output("purchase-history-graph-placeholder", "children"),
+    Input("manual-renew", "n_clicks"),
+    Input("auto-renew", "n_clicks"),
+    Input("male-gender", "n_clicks"),
+    Input("female-gender", "n_clicks"),
+    Input("age-range-filter", "value"),
+    Input("1-month-filter", "n_clicks"),
+    Input("3-month-filter", "n_clicks"),
+    Input("6-month-filter", "n_clicks"),
+    Input("All-time-filter", "n_clicks"),
+)
+def update_rating_overtime_graph(manual_clicks, auto_renew_clicks, male_clicks, female_clicks, age_range, month1, month3, month6, all_time):
+
+    return updated_purchase_hirtory_graph
+
+
+# callback for user engagement graph
+@callback(
+    Output("user-engagement-graph-placeholder", "children"),
+    Input("manual-renew", "n_clicks"),
+    Input("auto-renew", "n_clicks"),
+    Input("male-gender", "n_clicks"),
+    Input("female-gender", "n_clicks"),
+    Input("age-range-filter", "value"),
+    Input("1-month-filter", "n_clicks"),
+    Input("3-month-filter", "n_clicks"),
+    Input("6-month-filter", "n_clicks"),
+    Input("All-time-filter", "n_clicks"),
+)
+def update_user_engagement_graph(manual_clicks, auto_renew_clicks, male_clicks, female_clicks, age_range, month1, month3, month6, all_time):
+
+    return updated_user_engagement_graph
+
+
+
+if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
