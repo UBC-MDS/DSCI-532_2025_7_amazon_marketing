9376fbcb14bce86c8d800756a6bb93d827048eb2
Mavis WONG <mavis.295@gmail.com>
2025-03-09 20:42:35 -0700

    Render charts

--- src/components/charts.py
+++ src/components/charts.py
@@ -1,25 +1,26 @@
 import dash_bootstrap_components as dbc
 import dash_vega_components as dvc
+from dash import html
 
-Ratings_chart = dbc.Card(
+Ratings_chart = html.Div([dbc.Card(
     [dbc.CardHeader('User Ratings Overview',
                     style={"textAlign": "left",
                            'paddingLeft': '20px',
                            'fontWeight': 'bold'}),
      dbc.CardBody(dvc.Vega(id='rating_graph', spec={}))],
-     style={'width': '100%','height': '32%', 'marginTop': '15px'})
+     style={'width': '100%','height': '32%', 'marginTop': '15px'})])
 
-Purchase_history_chart = dbc.Card(
+Purchase_history_chart = html.Div([dbc.Card(
     [dbc.CardHeader('User Purchase History',
                     style={"textAlign": "left",
                           'paddingLeft': '20px', 
                           'fontWeight': 'bold'}),
      dbc.CardBody(dvc.Vega(id='purchase_graph', spec={}))],
-     style={"marginTop": "15px", 'width': '100%', 'height': '32%'})
+     style={"marginTop": "15px", 'width': '100%', 'height': '32%'})])
 
-Engagement_chart = dbc.Card(
+Engagement_chart = html.Div([dbc.Card(
     [dbc.CardHeader('User Engagement Levels',
                     style={"textAlign": "left",
                            'fontWeight': 'bold'}),
     dbc.CardBody(dvc.Vega(id='engagement_graph', spec={}))],
-    style={"marginTop": "15px", 'width': '100%', 'height': '26%'})
+    style={"marginTop": "15px", 'width': '100%', 'height': '26%'})])
