ab4bda994ef70f32892e8b81bc1869b47408a1ef
Mavis WONG <mavis.295@gmail.com>
2025-03-15 20:21:18 -0700

    Update data.py code to reduce unnecessary or slower computations

--- src/data/data.py
+++ src/data/data.py
@@ -6,11 +6,20 @@ import numpy as np
 data = pd.read_feather("data/raw/amazon_prime_users.feather")
 
 # Process the data
-data["Age"] = (pd.Timestamp.today() - data["Date of Birth"]).dt.days // 365
+today = pd.Timestamp.today()
+dates = ["Membership End Date", "Date of Birth"]
+cols = ['Gender',
+        'Renewal Status',
+        'Engagement Metrics',
+        "Purchase History"]
+
+
+data[cols] = data[cols].astype('category')
+data[dates] = data[dates].astype('date32[pyarrow]')
+
+data["Age"] = (today - data["Date of Birth"]).dt.days // 365
 data["Months Till Expire"] = np.ceil(
-    (data["Membership End Date"] - pd.Timestamp.today()).dt.days / 30).clip(lower=0)
-
-data["Membership End Date"] = data["Membership End Date"].dt.date
-
+    (data["Membership End Date"] - today).dt.days/30
+).clip(lower=0).astype('int')
 
 
