538dc95822b2dd646e2693cec967f0ca51091d46
Mavis WONG <mavis.295@gmail.com>
2025-03-15 22:52:56 -0700

    Load charts

--- src/components/charts.py
+++ src/components/charts.py
@@ -1,12 +1,13 @@
 import dash_bootstrap_components as dbc
 import dash_vega_components as dvc
+from dash import Dash, html, dcc
 
 Ratings_chart = dbc.Card(
     [dbc.CardHeader('User Ratings Overview',
                     style={"textAlign": "left",
                            'paddingLeft': '2vw',
                            'fontWeight': 'bold'}),
-     dbc.CardBody(dvc.Vega(id='rating_graph', spec={}, style={"height": '20vh'}))],
+     dbc.CardBody(dcc.Loading(dvc.Vega(id='rating_graph', spec={}, style={"height": '20vh'})))],
      style={'width': '100%','height': '29vh', 'marginTop': '2vh'})
 
 Purchase_history_chart = dbc.Card(
@@ -14,12 +15,12 @@ Purchase_history_chart = dbc.Card(
                     style={"textAlign": "left",
                           'paddingLeft': '2vw', 
                           'fontWeight': 'bold'}),
-     dbc.CardBody(dvc.Vega(id='purchase_graph', spec={}, style={"height": '20vh'}))],
+     dbc.CardBody(dcc.Loading(dvc.Vega(id='purchase_graph', spec={}, style={"height": '20vh'})))],
      style={"marginTop": "1.5vh", 'width': '100%', 'height': '29vh'})
 
 Engagement_chart = dbc.Card(
     [dbc.CardHeader('User Engagement Levels',
                     style={"textAlign": "left",
                            'fontWeight': 'bold'}),
-     dbc.CardBody(dvc.Vega(id='engagement_graph', spec={}, style={"height": '15vh'}))],
+     dbc.CardBody(dcc.Loading(dvc.Vega(id='engagement_graph', spec={}, style={"height": '15vh'})))],
     style={"marginTop": "1.5vh", 'width': '100%', 'height': '24vh'})
